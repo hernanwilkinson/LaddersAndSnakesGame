4dbd0d30617b43729e126a97ededa1e3b821100e
hernan.wilkinson <hernan.wilkinson@10pines.com>
2022-06-28 16:34:08 -0300

    Puede jugar más de un jugador

--- LaddersAndSnakesGame/LaddersAndSnakesGameTest.cs
+++ LaddersAndSnakesGame/LaddersAndSnakesGameTest.cs
@@ -34,12 +34,26 @@ namespace LaddersAndSnakesGame
             Assert.AreEqual(2,game.PositionOf("Player1"));
             Assert.AreEqual(1,game.PositionOf("Player2"));
         }
+        
+        [Test]
+        public void Test03()
+        {
+            var players = new List<object> {"Player1", "Player2"};
+            var simulatedDice = new SimulatedDice(new List<int> { 1, 3 });
+            var game = new Game(10,players,simulatedDice);
+
+            game.play();
+            game.play();
+
+            Assert.AreEqual(2,game.PositionOf("Player1"));
+            Assert.AreEqual(4,game.PositionOf("Player2"));
+        }
     }
 
      public class SimulatedDice
      {
          private readonly List<int> _rolls;
-         private int _currentRool = 0;
+         private int _currentRoll = 0;
          public SimulatedDice(List<int> rolls)
          {
              _rolls = rolls;
@@ -47,7 +61,7 @@ namespace LaddersAndSnakesGame
 
          public int roll()
          {
-             return _rolls[_currentRool++];
+             return _rolls[_currentRoll++];
          }
      }
 
@@ -56,9 +70,12 @@ namespace LaddersAndSnakesGame
          private readonly List<object> _players;
          private readonly SimulatedDice _dice;
          private IDictionary<object, int> _positionByPlayer = new Dictionary<object, int>();
+         private int _currentPlayerIndex;
+
          public Game(int numberOfCells, List<object> players, SimulatedDice dice)
          {
              _players = players;
+             _currentPlayerIndex = 0;
              _dice = dice;
              players.ForEach(player => _positionByPlayer.Add(player,1));
          }
@@ -71,7 +88,7 @@ namespace LaddersAndSnakesGame
          public void play()
          {
              var rolledNumber = _dice.roll();
-             var currentPlayer = _players[0];
+             var currentPlayer = _players[_currentPlayerIndex++];
              _positionByPlayer[currentPlayer] += rolledNumber;
          }
      }
