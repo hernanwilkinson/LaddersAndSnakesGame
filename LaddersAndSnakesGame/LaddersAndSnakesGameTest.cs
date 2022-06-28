721e53f3a0b733e620d85e4592b73f9e9ae56cad
hernan.wilkinson <hernan.wilkinson@10pines.com>
2022-06-28 16:31:43 -0300

    Se mueve el primer jugador

--- LaddersAndSnakesGame/LaddersAndSnakesGameTest.cs
+++ LaddersAndSnakesGame/LaddersAndSnakesGameTest.cs
@@ -21,26 +21,58 @@ namespace LaddersAndSnakesGame
             Assert.AreEqual(1,game.PositionOf("Player1"));
             Assert.AreEqual(1,game.PositionOf("Player2"));
         }
+        
+        [Test]
+        public void Test02()
+        {
+            var players = new List<object> {"Player1", "Player2"};
+            var simulatedDice = new SimulatedDice(new List<int> { 1 });
+            var game = new Game(10,players,simulatedDice);
+
+            game.play();
+            
+            Assert.AreEqual(2,game.PositionOf("Player1"));
+            Assert.AreEqual(1,game.PositionOf("Player2"));
+        }
     }
 
      public class SimulatedDice
      {
+         private readonly List<int> _rolls;
+         private int _currentRool = 0;
          public SimulatedDice(List<int> rolls)
          {
-             
+             _rolls = rolls;
+         }
+
+         public int roll()
+         {
+             return _rolls[_currentRool++];
          }
      }
 
      public class Game
      {
+         private readonly List<object> _players;
+         private readonly SimulatedDice _dice;
+         private IDictionary<object, int> _positionByPlayer = new Dictionary<object, int>();
          public Game(int numberOfCells, List<object> players, SimulatedDice dice)
          {
-             
+             _players = players;
+             _dice = dice;
+             players.ForEach(player => _positionByPlayer.Add(player,1));
          }
 
          public int PositionOf(object aPlayer)
          {
-             return 1;
+             return _positionByPlayer[aPlayer];
+         }
+
+         public void play()
+         {
+             var rolledNumber = _dice.roll();
+             var currentPlayer = _players[0];
+             _positionByPlayer[currentPlayer] += rolledNumber;
          }
      }
 }
